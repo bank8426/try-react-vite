0af57c3eee9b85633fa65adf2a73bc66020b0b6d
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-05-21 15:30:31 +0700

    query and display todo list in App, handle delete, add redirect to edit page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header"
+import MyImage from "./components/MyImage"
+import Checkbox from "./components/Checkbox"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from "react-router"
+
+const BASE_URL = import.meta.env.VITE_MOCKAPI_BASE_URL
+console.log(BASE_URL);
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [todoList, setTodoList] = useState([])
+  const getTodoList = async () => {
+    try {
+      let result = await axios.get(`${BASE_URL}/todos`)
+      console.log("result");
+      console.log(result);
 
+      if (result?.status === 200) {
+
+        setTodoList(result.data)
+      }
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+
+  }
+
+  const deleteTodo = async (id) => {
+    try {
+      let result = await axios.delete(`${BASE_URL}/todos/${id}`)
+      console.log("result");
+      console.log(result);
+
+      if (result?.status === 200) {
+        getTodoList()
+
+      }
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+  }
+
+
+  useEffect(() => {
+    getTodoList()
+
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <Header />
+      <div>Yo
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyImage imgUrl="https://fastly.picsum.photos/id/467/200/200.jpg?hmac=1DxsR7q_iuL1x5DGvv6KSaWR1rN1aawK2Iz0mZx40Mk" /> */}
+
+      {
+        todoList.map((item, index) => (
+          <div key={`todo-${index}`}>
+            {item.id} {item.name} {item.status}
+
+            <Link to={`edit/${item.id}`}><button>Edit</button></Link>
+
+
+            <button onClick={() => deleteTodo(item.id)}>Delete</button>
+
+          </div >
+
+        )
+        )
+      }
+
     </>
   )
 }
